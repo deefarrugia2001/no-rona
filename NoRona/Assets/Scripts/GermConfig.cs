64d96ad7a8099f0d878e9931334dcc055d99d2f1
Mandy Farrugia <87620808+deefarrugia2001@users.noreply.github.com>
2021-10-03 22:48:55 +0200

    Imported System.Collections.Generic in GermConfig.
    This is so a list of waypoints from a path prefab can be fetched.

--- NoRona/Assets/Scripts/GermConfig.cs
+++ NoRona/Assets/Scripts/GermConfig.cs
@@ -1,5 +1,14 @@
+using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "Germ Config")]
 public class GermConfig : ScriptableObject
 {
+    [SerializeField] GameObject germPrefab;
+    [SerializeField] GameObject pathPrefab;
+    [SerializeField] sbyte germCount;
+    [SerializeField] float secondsBetweenSpawns;
+    [SerializeField] float movementSpeed;
+
+    public GameObject Germ => germPrefab;
 }
