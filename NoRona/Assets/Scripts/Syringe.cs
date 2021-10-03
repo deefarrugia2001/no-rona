4bd05fb1f2f18e8bc9ed26462ff29051595605c5
Mandy Farrugia <87620808+deefarrugia2001@users.noreply.github.com>
2021-10-03 10:50:49 +0200

    The liquid prefab has been set to be fetched via runtime.
    C# code has been written to ensure that instead of loading the liquid prefab via Inspector, it will instead be loaded via runtime.

--- NoRona/Assets/Scripts/Syringe.cs
+++ NoRona/Assets/Scripts/Syringe.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Syringe : MonoBehaviour
 {
+    [SerializeField] GameObject liquid; //Liquid represents the projectile to emerge from the syringe in order to destroy the germs.
+
     void Start()
     {
-        
+        liquid = Resources.Load<GameObject>("Prefabs/Liquid"); //Instead of loading the prefab via the Inspector, it will be loaded amidst runtime from Resources/Prefabs whereby Liquid.prefab will be fetched.
     }
 
     void Update()
     {
-        
     }
 }
