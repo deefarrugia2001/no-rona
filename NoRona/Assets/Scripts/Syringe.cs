ab98b90aacc02979b6034e93e936fb7b84c1e327
Mandy Farrugia <87620808+deefarrugia2001@users.noreply.github.com>
2021-10-03 12:48:16 +0200

    The syringe is now able to move horizontally at a frame-independent rate.
    CheckBoundaries() has been set to limit the horizontal movement to the walls of the camera.

--- NoRona/Assets/Scripts/Syringe.cs
+++ NoRona/Assets/Scripts/Syringe.cs
@@ -6,12 +6,55 @@ public class Syringe : MonoBehaviour
 {
     [SerializeField] GameObject liquid; //Liquid represents the projectile to emerge from the syringe in order to destroy the germs.
 
+    [SerializeField] float movementSpeed = 10f;
+
+    Coroutine injectCoroutine;
+    float injectingTime = .3f;
+
+    float xMin, xMax;
+    float padding = 1f;
+
     void Start()
     {
         liquid = Resources.Load<GameObject>("Prefabs/Liquid"); //Instead of loading the prefab via the Inspector, it will be loaded amidst runtime from Resources/Prefabs whereby Liquid.prefab will be fetched.
+        CheckBoundaries();
     }
 
     void Update()
     {
+        Move();
+        Inject();
+    }
+
+    void Move() 
+    {
+        float horizontalAxis = Input.GetAxis("Horizontal"); //Return the horizontal axis as set in Input Settings.
+        Vector2 movement = Vector2.right * horizontalAxis; //Fetch horizontal movement only.
+        transform.Translate(movementSpeed * Time.deltaTime * movement); //Move the syringe horizontally at a frame-independent rate.
+
+        Vector3 boundaryMovement = transform.position; //Fetch the current position of the syringe.
+        float clampedHorizontalMovement = Mathf.Clamp(boundaryMovement.x, xMin, xMax); //Limit the horizontal movement of the syringe to the xMin and xMax values set in CheckBoundaries (invoked in the built-in Start()).
+        boundaryMovement = new Vector3(clampedHorizontalMovement, transform.position.y, transform.position.z); //Leave the y and z positions intact.
+        transform.position = boundaryMovement; //Update the current position with the clamped horizontal movement.
+    }
+
+    void CheckBoundaries() 
+    {
+        Camera camera = Camera.main; //Fetch the main camera.
+        xMin = camera.ViewportToWorldPoint(new Vector3(0,0,0)).x + padding;
+        xMax = camera.ViewportToWorldPoint(new Vector3(1,0,0)).x - padding;
+    }
+
+    void Inject() 
+    {
+        if(Input.GetButtonDown("Fire1")) 
+            injectCoroutine = StartCoroutine(InjectContinuously());
+        if (Input.GetButtonUp("Fire1"))
+            StopCoroutine(injectCoroutine);
+    }
+
+    IEnumerator InjectContinuously() 
+    {
+        yield return new WaitForSeconds(injectingTime);
     }
 }
